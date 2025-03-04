9280a392312428602afae3a5d5ae3cc145c7ce0b
Seif-Sallam <Seif.0100671.CS22@fcai.usc.edu.eg>
2025-03-04 06:27:16 +0200

    Update Program.cs

--- Quize_Game/Program.cs
+++ Quize_Game/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Quize_Game
+namespace Quize_Game
 {
     class Program
     {
@@ -22,8 +22,8 @@
             Console.WriteLine("Welcome To My Game");
             Console.WriteLine("-------------------");
             Console.WriteLine("Please Answer The Following Question :\n");
-
-
+            int score = 0;
+            int index = 0;
             for(int i =0;i<questions.Length;i++)
             {
                 Console.WriteLine($"{questions[i]}\n");
@@ -33,17 +33,38 @@
                     bool result = CheckAnswer(userAnswer, answers[i]);
                     if (result == true)
                     {
-                        Console.WriteLine("Correct Answer✔️");
+                        Console.WriteLine("Correct Answer✔️.");
+                        score++;
                     }
                     else
                     {
-                        Console.WriteLine($"Sorry‼️, Incorrect Answer, The Correct Answer IS: {answers[i]} ");
+                        Console.WriteLine($"Sorry‼️, Incorrect Answer, The Correct Answer IS: {answers[i]}. ");
                     }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
+                index = i+1;
+            }
+            if (index - score == 0)
+            {
+                Console.WriteLine($"Your Score IS: {score}🥇🏆, Congratulations.");
+            }
+            else if (index - score == 1)
+            {
+                Console.WriteLine($"Your Score IS: {score}🥈.");
+
+            }
+
+            else if (index - score == 2)
+            {
+                Console.WriteLine($"Your Score IS: {score}🥉.");
+
+            }
+            else
+            {
+                Console.WriteLine($"Your Score IS: {score}😔, Good Luke In The Next Game.");
 
             }
             Console.ReadKey();
